3aa9004b7842544b2626fbdcb4567a7e49b3cab0
WAKIL MAHMUD RAHAT <wakil_mahmud_rahat@WAKILs-MacBook-Air.local>
2026-02-03 13:50:50 +0600

    final js logic implemented

--- script.js
+++ script.js
@@ -1,40 +1,52 @@
 let coinAvailable = parseInt(document.getElementById("coinBtn").innerText)
 
 const callCost = 20;
-
-const headLine = document.getElementById("headline").innerText
-const numValue = document.getElementById("num").innerText
-let heartCount = parseInt(document.getElementById("heartCounting").innerText)
+let heartCount = parseInt(document.getElementById("heartCounting").innerText);
 
 
-document.getElementById("callBtn").addEventListener("click", function (e){
-
-if(coinAvailable<=0){
-    alert("you can not call, add coin");
-    return;
-}
-
-    alert("Calling")
-    coinAvailable = coinAvailable - callCost;
-    
-
-    document.getElementById("coinBtn").innerText = coinAvailable;
+let copyCount = parseInt(document.getElementById("copyCount").innerText);
 
 
-    document.getElementById("historyBtn").innerHTML += `
-    
+
+document.getElementById("cardContainer").addEventListener("click", (e)=>{
+    if(e.target.closest(".callBtn")){
+
+        //not event delegation just delegation inside evnet delegation
+        // take parent first 
+        // then aim child within
+        //delegating → then delegating again inside the handler.
+        //this is how we get inside element of class by delegation
+        const card = e.target.closest(".a-card");
+        const title = card.querySelector(".headline").innerText
+        const number = card.querySelector(".num").innerText
+
+        if(coinAvailable<callCost){
+            alert("not enough coin, add coin")
+
+            return
+        }
+        alert("calling")
+        coinAvailable = coinAvailable - callCost;
+
+        document.getElementById("coinBtn").innerText = coinAvailable;
+
+
+        document.getElementById("historyBtn").innerHTML += `
         <div class="flex justify-between p-6 m-4 border border-gray-500/5 rounded-2xl bg-gray-500/6">
                         
                         <div>
-                        <h1>${headLine}</h1>
-                        <p>${numValue}</p>
+                        <h1>${title}</h1>
+                        <p>${number}</p>
                         </div>
 
                         <div>
                             <p>${new Date().toLocaleTimeString()}</p>
                         </div>
                         </div>
-    `
+        `
+        
+
+    }
     
     
 })
@@ -45,11 +57,32 @@ document.getElementById("clearBtn").addEventListener("click", ()=>{
 })
 
 
-
-document.getElementById("heartBtn").addEventListener("click", (e) => {
-
-    heartCount += 1;
+document.addEventListener("click",(e)=>{
+    if(e.target.closest(".heartBtn")){
+        heartCount += 1;
 
     document.getElementById("heartCounting").innerText = heartCount;
-    
+
+    }
 })
+
+
+document.addEventListener("click",(e)=>{
+    if(e.target.closest(".copyBtn")){
+
+
+        //as the .num is a class and we want its inside content we need delegation again
+        const card = e.target.closest(".a-card");
+        const copyNum = card.querySelector(".num").innerText;
+
+        navigator.clipboard.writeText(copyNum);
+        alert("copied")
+
+
+        copyCount += 1;
+
+
+        document.getElementById("copyCount").innerText = copyCount;
+    }
+})
+
